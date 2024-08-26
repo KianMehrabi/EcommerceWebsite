64d0f95f8c210ddf2172b7307849ee799902ca72
KianMehrabi <gmkianmehrabi@gmail.com>
2024-08-26 21:59:29 +0330

    adding the register page and files to project

--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 # importing from views
-from .views import *
+from . import views
 
 from . import views
 
 urlpatterns = [
-    path("", home_page, name="home"),
-    path("about/", about_page, name="about"),
-    path("login/", login_user, name="login"),
+    path("", views.home_page, name="home"),
+    path("about/", views.about_page, name="about"),
+    path("login/", views.login_user, name="login"),
+    path("logout/", views.logout_user, name="logout"),
+    path("register/", views.register_user, name="register"),
 ]
--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,27 +1,42 @@
-from django.shortcuts import render , redirect
-from django.contrib.auth import authenticate , login , logout
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from .models import Product
 
+
 def home_page(request):
     products = Product.objects.all()
-    return render(request , "pages/home.html" , {"products":products})
+    return render(request, "pages/home.html", {"products": products})
+
 
 def about_page(request):
-    return render(request ,"pages/about.html" , {})
+    return render(request, "pages/about.html", {})
+
 
 def login_user(request):
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request , username = username , password = password)
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request , user)
-            messages.success(request , ("You Have Been Loged In"))
-            return redirect('home')
+            login(request, user)
+            messages.success(request, ("You Have Been Loged In"))
+            return redirect("home")
         else:
-            messages.success(request , ("There was a Error"))
-            return redirect('login')
+            messages.success(request, ("There was a Error"))
+            return redirect("login")
     else:
-        return render(request , "pages/login.html" , {})
+        return render(request, "pages/login.html", {})
+
+
+@login_required(login_url="login")
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You Have Been Loged out"))
+    return redirect("home")
+
+
+def register_user(request):
+    return render(request, "pages/register.html", {})
