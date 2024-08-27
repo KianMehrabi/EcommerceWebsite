44ffd5cad742cddf3147b769fa70ab5bd5afcad1
KianMehrabi <gmkianmehrabi@gmail.com>
2024-08-27 11:51:45 +0330

    customizing the form and adding it to page

--- ecom/store/forms.py
+++ ecom/store/forms.py
@@ -6,3 +6,15 @@ class CreatingUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "password1", "password2"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Please Enter Username"}
+        )
+        self.fields["password1"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Please Enter Password"}
+        )
+        self.fields["password2"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Repeat Password"}
+        )
